77351474f90ffcd931b0c46847998da4f1350875
Jarne <descheerderjarne@gmail.com>
2025-05-02 19:46:11 +0200

    début + json

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,39 @@
-console.info('Hello world');
+"use strict";
+
+/* menu */
+
+var menuBtn = document.querySelector(".menu__btn");
+var menuElements = document.querySelectorAll(".menu__li");
+
+menuBtn.addEventListener("click", toggleMenu);
+menuElements.forEach((element) => {
+    element.addEventListener("click", toggleMenu);
+});
+
+function toggleMenu() {
+    var menu = document.querySelector(".menu");
+    menu.classList.toggle("menu--open");
+};
+
+// afficher données
+
+window.onload = function() {
+    fetch('../assets/countries-FR.json')
+    .then(response => response.json)
+    .then(data => {
+        displayPays(data);
+    });
+};
+
+function displayPays(data) {
+    const selection = document.getElementById('pays');
+
+    for(const code in data) {
+        if(data.hasOwnProprety(code)) {
+            let option = document.createElement('option');
+            option.value = code;
+            option.textContent = data[code];
+            selection.appendChild(option);
+        }
+    }
+}
