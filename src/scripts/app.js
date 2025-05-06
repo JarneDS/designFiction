06c5cb36c632c846943419473dc593dd1f94c1a2
Jarne <descheerderjarne@gmail.com>
2025-05-06 16:01:48 +0200

    suiteDesIgnFiction
    ajout page contact + lien avec image HEAJ + suite page test + js + scss

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -33,8 +33,58 @@ function displayPays(data) {
             let option = document.createElement('option');
             option.value = code;
             option.textContent = data[code];
+            // Définir "Belgique" comme valeur par défaut
+            if(option.textContent === "Belgique") {
+                option.selected = true;
+            }
             selection.appendChild(option);
         }
     }
     // fin code copilot
 }
+
+const envoyer = document.querySelector(".submit");
+envoyer.addEventListener("click", displayResult);
+
+function displayResult(event) {
+    event.preventDefault();
+
+    var montantInput = document.getElementById('quantite');
+    var montant = document.getElementById('montant');
+    montant.innerHTML = "";
+
+    // afficher la div des résultats
+    const divResult = document.querySelector(".resultatsRemerciement");
+    divResult.classList.add("div--afficher");
+
+    // afficher les résultats
+    var montantValue = parseInt(montantInput.value, 10);
+
+    if (montantValue >= 0 && montantValue <= 200) {
+        montant.innerHTML += " trop peu";
+    }
+
+    else if (montantValue >= 201 && montantValue <= 400) {
+        montant.innerHTML += " peu";
+    }
+    else if (montantValue >= 401 && montantValue <= 600) {
+        montant.innerHTML += " moyenne";
+    }
+    else if (montantValue >= 601 && montantValue <= 800) {
+        montant.innerHTML += " bonne";
+    }
+
+    else if (montantValue >= 801 && montantValue <= 1000) {
+        montant.innerHTML += " très bonne";
+    }
+
+    else if (montantValue >= 1001) {
+        montant.innerHTML += " superbe";
+    }
+
+    else {
+        montant.innerHTML += "";
+    }
+}
+
+
